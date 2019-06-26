79817584ce38c43ba963b3050fe5ddd4b9558634
jolifee <joliff.julie@hotmail.com>
2019-06-26 10:33:47 +0200

    creation module Meteo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <meteo />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import meteo from './components/meteo.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    meteo
   }
 }
 </script>
